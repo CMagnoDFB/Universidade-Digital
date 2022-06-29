f9ccdc29edcda4538ca839107960e90a29edbb70
Rodrigo da Silva Freitas <rodrigojato@hotmail.com>
2022-06-28 21:45:03 -0300

    Pagina inicial reformada

--- app/screens/PostsScreen.js
+++ app/screens/PostsScreen.js
@@ -1,53 +1,59 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, FlatList } from "react-native";
 import AppButton from "../components/AppButton";
-import PostSeparator from "../components/PostSeparator";
+import { Icon } from 'react-native-elements';
 import Post from "../components/Post";
-import Constants from "expo-constants";
 
 import { checkLoginState, removeLoginState, saveUserObject, getUserObject } from "./../../loginState"
+import { ScrollView } from "react-native-gesture-handler";
 
 const posts = [
   {
     id: 1,
-    user: "Fulado de Tal",
-    title: "Alguém sabe me informar isso?",
-    tag: "Informação",
+    user: "Fulano de Tal",
+    role: "Estudante de Ciência da Computação",
+    date: "há 10 horas",
+    tags: ["Métodos Numéricos II"],
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 2,
     user: "Cicrano",
-    title: "Aviso pra quem come no RU",
-    tag: "RU",
+    role: "Professor do DC",
+    date: "há 10 horas",
+    tags: ["CANA"],
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 3,
     user: "Cicrano",
-    title: "Aviso pra quem come no RU",
-    tag: "RU",
+    role: "Estudante de Ciência da Computação",
+    date: "há 10 horas",
+    tags: ["SGBD"],
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 4,
     user: "Cicrano",
-    title: "Aviso pra quem come no RU",
-    tag: "RU",
+    role: "Estudante de Ciência da Computação",
+    date: "há 10 horas",
+    tags: ["Métodos Numéricos II"],
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 5,
     user: "Beltrano",
-    title: "Aviso pra quem come no RU",
-    tag: "RU",
+    role: "Estudante de Ciência da Computação",
+    date: "há 10 horas",
+    tags: ["Métodos Numéricos II"],
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 6,
     user: "Cicrano",
-    title: "Aviso pra quem come no RU",
-    tag: "RU",
+    role: "Estudante de Ciência da Computação",
+    date: "há 10 horas",
+    tags: ["Engenharia de Software"],
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
 ];
@@ -92,47 +98,73 @@ export default function PostsScreen({ navigation }) {
 
   return (
     <>
-    <View style={styles.buttonsContainer}>
-      <AppButton
-        title="Sair"
-        onPress={() => efetuarLogout()}
-        color="media2"
-      />
-    </View>
-    <View style={styles.buttonsContainer}>
-      <AppButton
-        title="Editar perfil"
-        onPress={() => irEdicaoPerfil()}
-        color="media2"
-      />
-    </View>
-    
-    <FlatList
-      style={styles.screen}
-      data={posts}
-      keyExtractor={(post) => post.id.toString()}
-      renderItem={({ item: post }) => (
-        <Post
-          title={post.title}
-          tag={post.tag}
-          body={post.body}
-          user={post.user}
+    <ScrollView>
+      <View style={styles.buttonsContainer}>
+        <Icon
+          onPress={() => efetuarLogout()}
+          name='sign-out'
+          type='font-awesome'
+          color='#000'
+          raised
+          size={25}
+          style={styles.headerIcon}
         />
-      )}
-      ItemSeparatorComponent={PostSeparator}
-    />
+        <Icon
+          onPress={() => irEdicaoPerfil()}
+          name='edit'
+          type='font-awesome'
+          color='#000'
+          raised
+          size={25}
+          style={styles.headerIcon}
+        />
+        <Icon
+          onPress={() => console.log("a ser feito..")}
+          name='user'
+          type='font-awesome'
+          color='#000'
+          raised
+          size={25}
+          style={styles.headerIconRight}
+        />
+      </View>
+      
+      <View >
+        { 
+          posts.map((post, postKey) => {
+            return (
+              <Post
+                key={postKey}
+                role={post.role}
+                tags={post.tags}
+                body={post.body}
+                user={post.user}
+                date={post.data_pub}
+              />
+            );
+          })
+        }
+      </View>
+
+    </ScrollView>
+    
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    paddingTop: Constants.statusBarHeight,
-  },
   buttonsContainer: {
     width: "100%",
-    paddingHorizontal: "20%",
-  }
+    flexDirection: "row",
+    marginHorizontal: 20,
+    
+  },
+  headerIcon: {
+    flexDirection: "column",
+  },
+  headerIconRight: {
+    flexDirection: "column",
+  },
 });
 
 // Para simplificar o projeto, creio que devamos limitar uma tag por post
