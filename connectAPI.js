b98961b3441043d1bca1b7b5ede9c6be12eb01bd
Rodrigo da Silva Freitas <rodrigojato@hotmail.com>
2022-07-04 14:03:20 -0300

    Tela de view de Posts, criar e apagar replies Correcao

--- connectAPI.js
+++ connectAPI.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
 const api = axios.create({
-    //baseURL: "https://api-uni-digital.herokuapp.com:443/"
+    baseURL: "https://api-uni-digital.herokuapp.com:443/"
     //baseURL: "http://192.168.0.59:80/"   // Fortaleza
-    baseURL: "http://192.168.0.101:80/"  // Caucaia
+    //baseURL: "http://192.168.0.101:80/"  // Caucaia
 })
 
 export default api;
